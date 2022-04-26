2295d87b61db3e846b888692486d6e65f73bc3d8
Bo <wenbostar@gmail.com>
2022-04-26 16:05:08 -0500

    Update run_report.R

--- run_report.R
+++ run_report.R
@@ -1,5 +1,5 @@
 library("rmarkdown")
-run_reporter=function(x,out_dir="./",prefix="test"){
+run_reporter=function(x,out_dir="./",prefix="test",min_n_sample_name=50){
     rmd <- "report.rmd"
     x <- normalizePath(x)
     out_dir <- normalizePath(out_dir)
@@ -7,7 +7,8 @@ run_reporter=function(x,out_dir="./",prefix="test"){
     cat("Input file:",x,"\n")
     cat("Work dir:",work_dir,"\n")
 	out_file <- paste0(out_dir,"/report.html")
-    render(rmd,params = list(input_dir=x,out_dir=out_dir,prefix=prefix),output_file = basename(out_file),
+    render(rmd,params = list(input_dir=x,out_dir=out_dir,prefix=prefix,min_n_sample_name=min_n_sample_name),
+	   output_file = basename(out_file),
            output_dir = work_dir,
            intermediates_dir = work_dir,
            knit_root_dir = getwd(),
